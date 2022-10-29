b4c3a76f4dae2dd3ab9ded5ec3b6adf9a96014ae
jretondo <jretondo90@gmail.com>
2022-10-29 19:44:11 -0300

    add proof

--- src/routes/Test/HeroSliderAdmin/EliminarHero.js
+++ src/routes/Test/HeroSliderAdmin/EliminarHero.js
@@ -26,7 +26,7 @@ EliminarHero.post("/test/delete-hero-id", async (req, res) => {
             const imageName = idHero + ".jpg"
             const directory = path.join(__dirname, '..', '..', '..', '..', 'Public', 'Imagenes', 'HeroSlider', imageName)
             console.log('directory :>> ', directory);
-            fs.rmdir(directory)
+            fs.rm(directory)
 
             respuesta = {
                 status: 200,
